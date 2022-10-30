0bcd3a2cd9a1081c89be7c59684fe4e5b4f13bd4
Chris Hager <chris@linuxuser.at>
2022-10-30 13:49:53 +0000

    add builder0x69 relay

--- common/relays.go
+++ common/relays.go
@@ -8,6 +8,7 @@ var RelayURLs = []string{
 	"https://0x9000009807ed12c1f08bf4e81c6da3ba8e3fc3d953898ce0102433094e5f22f21102ec057841fcb81978ed1ea0fa8246@builder-relay-mainnet.blocknative.com",
 	"https://0xb3ee7afcf27f1f1259ac1787876318c6584ee353097a50ed84f51a1f21a323b3736f271a895c7ce918c038e4265918be@relay.edennetwork.io",
 	"https://0x98650451ba02064f7b000f5768cf0cf4d4e492317d82871bdc87ef841a0743f69f0f1eea11168503240ac35d101c9135@mainnet-relay.securerpc.com",
+	"https://0x84e78cb2ad883861c9eeeb7d1b22a8e02332637448f84144e245d20dff1eb97d7abdde96d4e7f80934e5554e11915c56@relayooor.wtf",
 }
 
 func GetRelays() ([]RelayEntry, error) {
