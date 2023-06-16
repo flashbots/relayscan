00904df06758d63e6353ad106f3a091f86b9293e
Chris Hager <chris@linuxuser.at>
2023-06-16 09:01:44 +0200

    scripts cleanup

--- scripts/backfill-stats.sh
+++ scripts/backfill-stats.sh
@@ -4,4 +4,4 @@ dir=$( dirname -- "$0"; )
 cd $dir
 cd ..
 source .env.prod
-/server/relayscan/relayscan core update-builder-stats --backfill --daily --verbose 2>&1 | /usr/bin/tee -a /var/log/relayscan-stats.log
+./relayscan core update-builder-stats --backfill --daily --verbose 2>&1 | /usr/bin/tee -a /var/log/relayscan-stats.log
