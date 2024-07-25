fcb00990aebc5eec8f6c61445285ce1eec36c42d
Chris Hager <chris@linuxuser.at>
2024-07-25 14:56:28 +0200

    geth update and minor cleanup

--- cmd/util/util.go
+++ cmd/util/util.go
@@ -4,6 +4,8 @@ package util
 import (
 	"github.com/flashbots/relayscan/common"
 	"github.com/spf13/cobra"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 var (
@@ -11,10 +13,16 @@ var (
 	numThreads uint64 = 10
 
 	// Printer for pretty printing numbers
-	// printer = message.NewPrinter(language.English)
+	printer = message.NewPrinter(language.English)
 
 	ethNodeURI       string
 	ethNodeBackupURI string
+	slotStr          string
+	blockHash        string
+	mevGethURI       string
+	loadAddresses    bool
+	scLookup         bool // whether to lookup smart contract details
+	printAllSimTx    bool
 )
 
 var UtilCmd = &cobra.Command{
