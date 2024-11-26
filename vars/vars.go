851cc71ba2452074803735ababceb63c519cc1ee
richard <ohko4711@163.com>
2024-11-26 11:52:14 +0800

    feat: support read Genesis from env for custom chain

--- vars/vars.go
+++ vars/vars.go
@@ -3,6 +3,7 @@ package vars
 
 import (
 	"os"
+	"strconv"
 
 	relaycommon "github.com/flashbots/mev-boost-relay/common"
 )
@@ -11,7 +12,7 @@ var (
 	Version  = "dev" // is set during build process
 	LogDebug = os.Getenv("DEBUG") != ""
 	LogJSON  = os.Getenv("LOG_JSON") != ""
-	Genesis  = 1_606_824_023
+	Genesis  = getGenesis()
 
 	DefaultBeaconURI        = relaycommon.GetEnv("BEACON_URI", "http://localhost:3500")
 	DefaultPostgresDSN      = relaycommon.GetEnv("POSTGRES_DSN", "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
@@ -19,3 +20,13 @@ var (
 	DefaultEthNodeURI       = relaycommon.GetEnv("ETH_NODE_URI", "")
 	DefaultEthBackupNodeURI = relaycommon.GetEnv("ETH_NODE_BACKUP_URI", "")
 )
+
+func getGenesis() int64 {
+	genesis := int64(1_606_824_023)
+	if envGenesis := os.Getenv("GENESIS"); envGenesis != "" {
+		if parsedGenesis, err := strconv.ParseInt(envGenesis, 10, 64); err == nil {
+			genesis = parsedGenesis
+		}
+	}
+	return genesis
+}
