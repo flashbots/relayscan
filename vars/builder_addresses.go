940ecfcc1e738cb348c1e8eba359837c34e9e33a
Andrés Babino <ababino@gmail.com>
2025-11-04 09:08:38 -0300

    add BuilderNet addresses (#70)

--- vars/builder_addresses.go
+++ vars/builder_addresses.go
@@ -3,5 +3,7 @@ package vars
 var BuilderAddresses = map[string]map[string]bool{ // coinbase: owned addresses
 	"0xdadb0d80178819f2319190d340ce9a924f783711": {
 		"0x59cadf9199248b50d40a6891c9e329ea13a88d31": true,
+		"0x75cc09358f100583d66f5277138bfb476345dc1b": true,
+		"0x397b28d85d77fef1576e129bb35b322c2bee1ba1": true,
 	},
 }
